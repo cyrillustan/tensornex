65e8f58f8d10fccdb29012fd37acc1030a59d00e
Cyrillus <cyztan@gmail.com>
2024-10-09 17:22:10 -0400

    Add coupled tensor partial least squares to it

---
+++ tensornex/test/test_tpls.py
@@ -0,0 +1,220 @@
+import pytest
+import numpy as np
+from numpy.testing import assert_allclose
+from sklearn.decomposition import PCA
+import tensorly as tl
+from tensorly.cp_tensor import CPTensor, cp_normalize
+from tensorly.metrics.factors import congruence_coefficient
+
+from tensornex.tpls import tPLS
+
+
+TENSOR_DIMENSIONS = (100, 38, 65)
+N_RESPONSE = 4
+N_LATENT = 8
+
+def make_synthetic_test(cp_tensor: tl.cp_tensor, test_samples: int,
+                        error: float = 0, seed: int = 215):
+    """
+    Generates test set from given factors.
+
+    Parameters:
+        cp_tensor (tl.cp_tensor): CP tensor
+        test_samples (int): samples in testing set
+        error (float, default: 0): standard error of added gaussian noise
+        seed (int, default: 215): seed for random number generator
+
+    Returns:
+        x_test (np.array): tensor of measurements
+        y_test (np.array): response variables
+        test_tensor (tl.cp_tensor): CP tensor of x_test, y_test
+    """
+    rng = np.random.default_rng(seed)
+
+    test_factors = cp_tensor.factors
+    test_factors[0] = rng.normal(0, 1, size=(test_samples, cp_tensor.rank))
+    test_tensor = tl.cp_tensor.CPTensor((None, test_factors))
+    test_tensor.y_factor = cp_tensor.y_factor
+
+    x_test = tl.cp_to_tensor(test_tensor)
+    x_test += rng.normal(0, error, size=test_tensor.shape)
+    y_test = tl.dot(test_tensor.factors[0], cp_tensor.y_factor.T)
+    y_test += rng.normal(0, error, size=y_test.shape)
+
+    return x_test, y_test, test_tensor
+
+
+def import_synthetic(train_dimensions: tuple, n_response: int, n_latent: int,
+                     error: float = 0, seed: int = 215):
+    """
+    Generates synthetic data.
+
+    Parameters:
+        train_dimensions (tuple): dimensions of training data
+        n_response (int): number of response variables
+        n_latent (int): number of latent variables in synthetic data
+        error (float, default: 0): standard error of added gaussian noise
+        seed (int, default: 215): seed for random number generator
+
+    Returns:
+        x (np.array): tensor of measurements
+        y (np.array): response variables
+        cp_tensor (tl.cp_tensor): CP tensor of x, y
+    """
+    rng = np.random.default_rng(seed)
+
+    x_factors = [rng.normal(0, 1, size=(train_dimensions[0], n_latent))]
+    y_factor = rng.normal(0, 1, size=(n_response, n_latent))
+
+    for dimension in train_dimensions[1:]:
+        x_factors.append(rng.normal(0, 1, size=(dimension, n_latent)))
+
+    cp_tensor = tl.cp_tensor.CPTensor((None, x_factors))
+    cp_tensor.y_factor = y_factor
+
+    x = tl.cp_to_tensor(cp_tensor)
+    x += rng.normal(0, error, size=train_dimensions)
+
+    y = tl.dot(cp_tensor.factors[0], cp_tensor.y_factor.T)
+    y += rng.normal(0, error, size=(train_dimensions[0], n_response))
+
+    if y.shape[1] == 1:
+        y = y.flatten()
+
+    return x, y, cp_tensor
+
+# Supporting Functions
+
+def _get_standard_synthetic():
+    x, y, cp_tensor = import_synthetic(TENSOR_DIMENSIONS, N_RESPONSE, N_LATENT)
+    pls = tPLS(N_LATENT)
+    pls.fit(x, y)
+    return x, y, cp_tensor, pls
+
+
+# Class Structure Tests
+
+def test_factor_normality():
+    x, y, _, pls = _get_standard_synthetic()
+    for x_factor in pls.X_factors[1:]:
+        assert_allclose(tl.norm(x_factor, axis=0), 1)
+    for y_factor in pls.Y_factors[1:]:
+        assert_allclose(tl.norm(y_factor, axis=0), 1)
+
+
+# This method should test for factor hyper-orthogonality; components seem
+# very loosely hyper-orthogonal (cut-off of 1E-2 is generous).
+def test_factor_orthogonality():
+    x, y, _, pls = _get_standard_synthetic()
+    x_cp = CPTensor((None, pls.X_factors))
+    x_cp = cp_normalize(x_cp)
+
+    for component_1 in range(x_cp.rank):
+        for component_2 in range(component_1 + 1, x_cp.rank):
+            factor_product = 1
+            for factor in x_cp.factors:
+                factor_product *= np.dot(
+                    factor[:, component_1],
+                    factor[:, component_2]
+                )
+            assert abs(factor_product) < 1E-2
+
+
+def test_consistent_components():
+    x, y, _, pls = _get_standard_synthetic()
+
+    for x_factor in pls.X_factors:
+        assert x_factor.shape[1] == N_LATENT
+
+    for y_factor in pls.Y_factors:
+        assert y_factor.shape[1] == N_LATENT
+
+
+# Dimension Compatibility Tests
+
+@pytest.mark.parametrize("idims", [(2, 1), (3, 1), (4, 1), (2, 4), (3, 4), (4, 4)])
+def _test_dimension_compatibility(idims):
+    x_rank, n_response = idims
+    x, y, _ = import_synthetic(tuple([100] * x_rank), n_response, N_LATENT)
+    try:
+        pls = tPLS(N_LATENT)
+        pls.fit(x, y)
+    except ValueError:
+        raise AssertionError(
+            f'Fit failed for {len(x.shape)}-dimensional tensor with '
+            f'{n_response} response variables in y'
+        )
+
+
+# Decomposition Accuracy Tests
+
+def test_same_x_y():
+    x, _, _ = import_synthetic((100, 100), N_RESPONSE, N_LATENT)
+    pls = tPLS(N_LATENT)
+    pca = PCA(N_LATENT)
+
+    pls.fit(x, x)
+    scores = pca.fit_transform(x)
+
+    assert_allclose(pls.X_factors[0], pls.Y_factors[0], rtol=0, atol=1E-4)
+    assert_allclose(pls.X_factors[1], pls.Y_factors[1], rtol=0, atol=1E-4)
+    assert congruence_coefficient(pls.X_factors[0], scores)[0] > 0.95
+    assert congruence_coefficient(pls.X_factors[1], pca.components_.T)[0] > 0.95
+
+
+def test_zero_covariance_x():
+    x, y, _ = import_synthetic(TENSOR_DIMENSIONS, N_RESPONSE, N_LATENT)
+    x[:, 0, :] = 1
+    pls = tPLS(N_LATENT)
+    pls.fit(x, y)
+
+    assert_allclose(pls.X_factors[1][0, :], 0)
+
+
+
+@pytest.mark.parametrize("idims", [(3, 1), (4, 1),  (3, 4), (4, 2)])
+def _test_decomposition_accuracy(idims):
+    x_rank, n_response = idims
+    x, y, true_cp = import_synthetic(tuple([100] * x_rank), n_response, N_LATENT)
+    pls = tPLS(N_LATENT)
+    pls.fit(x, y)
+
+    for pls_factor, true_factor in zip(pls.X_factors, true_cp.factors):
+        assert congruence_coefficient(pls_factor, true_factor)[0] > 0.95
+
+    assert congruence_coefficient(pls.Y_factors[1], true_cp.y_factor)[0] > 0.95
+
+
+def _test_increasing_R2X(X, Y, info=""):
+    tpls = tPLS(12)
+    tpls.fit(X, Y)
+    assert np.all(np.diff(tpls.R2X) >= 0.0), "R2X is not monotonically increasing"
+    assert np.all(np.diff(tpls.R2Y) >= 0.0), \
+        f"R2Y is not monotonically increasing. " \
+        f"Streak till {np.where(np.diff(tpls.R2Y) <= 0.0)[0][0] + 1}-th component, " \
+        f"R2Y = {tpls.R2Y[np.where(np.diff(tpls.R2Y) <= 0.0)[0][0]]}. " \
+        f"Y shape = {Y.shape}. {info}"
+
+@pytest.mark.parametrize("n_response", [5, 7, 9])
+def test_increasing_R2X_random(n_response):
+    X = np.random.rand(20, 8, 6, 4)
+    Y = np.random.rand(20, n_response)
+    _test_increasing_R2X(X, Y)
+
+@pytest.mark.parametrize("n_response", [5, 7, 9])
+def test_increasing_R2X(n_response, n_latent=5):
+    X, Y, _ = import_synthetic((20, 8, 6, 4), n_response, n_latent)
+    _test_increasing_R2X(X, Y, info=f"n_latent = {n_latent}")
+
+
+def test_transform():
+    """ Test transform the original X and Y will give the first factors """
+    X = np.random.rand(20, 8, 6, 4)
+    Y = np.random.rand(20, 5)
+    tpls = tPLS(6)
+    tpls.fit(X, Y)
+    rord = np.arange(20)
+    np.random.shuffle(rord)
+    X_scores, Y_scores = tpls.transform(X[rord, :], Y[rord, :])
+    assert np.allclose(X_scores, tpls.X_factors[0][rord, :])
+    assert np.allclose(Y_scores, tpls.Y_factors[0][rord, :])
---
+++ tensornex/tpls.py
@@ -0,0 +1,163 @@
+# include all packages, including those needed for the children classes
+from abc import ABCMeta
+from collections.abc import Mapping
+from copy import copy
+
+
+import numpy as np
+from numpy.linalg import norm, lstsq
+from tensorly.tenalg import multi_mode_dot, outer
+from tensorly.decomposition._cp import parafac
+from .utils import calcR2X, factors_to_tensor
+from .missingvals import miss_tensordot, miss_mmodedot
+
+
+class tPLS(Mapping, metaclass=ABCMeta):
+    """ Base class for all variants of tensor PLS """
+    def __init__(self, n_components:int):
+        super().__init__()
+        # Parameters
+        self.n_components = n_components
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.X_factors
+        elif index == 1:
+            return self.Y_factors
+        elif index == 2:
+            return self.coef_
+        else:
+            raise IndexError
+
+    def __iter__(self):
+        yield self.X_factors
+        yield self.Y_factors
+        yield self.coef_
+
+    def __len__(self):
+        return 3
+
+    def copy(self):
+        return copy(self)
+
+    def preprocess(self, X, Y):
+        # check input integrity
+        assert X.shape[0] == Y.shape[0]
+        assert Y.ndim <= 2, "Only a matrix (2-mode tensor) Y is acceptable."
+        if Y.ndim == 1:
+            Y = Y.reshape(-1, 1)
+
+        # mean center the data; set up factors
+        self.X_dim = X.ndim
+        self.X_shape = X.shape
+        self.Y_shape = Y.shape
+        self.X_factors = [np.zeros((l, self.n_components)) for l in X.shape]
+        self.Y_factors = [np.zeros((l, self.n_components)) for l in Y.shape]
+            # U takes the 1st column of Y
+        self.R2X = np.zeros((self.n_components))
+        self.R2Y = np.zeros((self.n_components))
+
+        self.X_hasMiss = np.any(np.isnan(X))
+        if self.X_hasMiss:
+            print("X has missing values")
+        self.X_miss = np.isnan(X)   # positions of missing value, not the opposite
+
+        self.X_mean = np.nanmean(X, axis=0)
+        self.Y_mean = np.nanmean(Y, axis=0)
+        self.coef_ = np.zeros((self.n_components, self.n_components))   # a upper triangular matrix
+        return X - self.X_mean, Y - self.Y_mean
+
+
+    def fit(self, X, Y, tol=1e-8, max_iter=100, verbose=0):
+        original_X, original_Y = X.copy(), Y.copy()
+        X, Y = self.preprocess(X, Y)
+        for a in range(self.n_components):
+            oldU = np.ones_like(self.Y_factors[0][:, a]) * np.inf
+            self.Y_factors[0][:, a] = Y[:, 0]
+            for iter in range(max_iter):
+                if self.X_hasMiss:
+                    Z = miss_tensordot(X, self.Y_factors[0][:, a], self.X_miss)
+                else:
+                    Z = np.einsum("i...,i...->...", X, self.Y_factors[0][:, a])
+                Z_comp = [Z / norm(Z)]
+                if Z.ndim >= 2:
+                    Z_comp = parafac(Z, 1, tol=tol, init="svd", normalize_factors=True)[1]
+                for ii in range(Z.ndim):
+                    self.X_factors[ii + 1][:, a] = Z_comp[ii].flatten()
+
+                if self.X_hasMiss:
+                    self.X_factors[0][:, a] = miss_mmodedot(X, [ff[:, a] for ff in self.X_factors[1:]], self.X_miss)
+                else:
+                    self.X_factors[0][:, a] = multi_mode_dot(X, [ff[:, a] for ff in self.X_factors[1:]], range(1, self.X_dim))
+                self.Y_factors[1][:, a] = Y.T @ self.X_factors[0][:, a]
+                self.Y_factors[1][:, a] /= norm(self.Y_factors[1][:, a])
+                self.Y_factors[0][:, a] = Y @ self.Y_factors[1][:, a]
+                if norm(oldU - self.Y_factors[0][:, a]) < tol:
+                    if verbose:
+                        print("Comp {}: converged after {} iterations".format(a, iter))
+                    break
+                oldU = self.Y_factors[0][:, a].copy()
+
+            X -= outer([ff[:, a] for ff in self.X_factors])
+            self.coef_[:, a] = lstsq(self.X_factors[0], self.Y_factors[0][:, a], rcond=-1)[0]
+            Y -= self.X_factors[0] @ self.coef_[:, [a]] @ self.Y_factors[1][:, [a]].T
+            # Y -= T b q' = T pinv(T) u q' = T lstsq(T, u) q'; b = inv(T'T) T' u = pinv(T) u
+            self.R2X[a] = calcR2X(original_X - self.X_mean, factors_to_tensor(self.X_factors))
+            self.R2Y[a] = calcR2X(original_Y - self.Y_mean, self.predict(original_X) - self.Y_mean)
+
+
+    def predict(self, X):
+        if self.X_shape[1:] != X.shape[1:]:
+            raise ValueError(f"Training X has shape {self.X_shape}, while the new X has shape {X.shape}")
+
+        X = X.copy() - self.X_mean
+        X_projection = np.zeros((X.shape[0], self.n_components))
+        X_hasMiss = np.any(np.isnan(X))
+        X_miss = np.isnan(X)
+
+        for a in range(self.n_components):
+            if X_hasMiss:
+                X_projection[:, a] = miss_mmodedot(X, [ff[:, a] for ff in self.X_factors[1:]], X_miss)
+            else:
+                X_projection[:, a] = multi_mode_dot(X, [ff[:, a] for ff in self.X_factors[1:]], range(1, self.X_dim))
+            X -= outer([X_projection[:, a]] + [ff[:, a] for ff in self.X_factors[1:]])
+        return X_projection @ self.coef_ @ self.Y_factors[1].T + self.Y_mean
+
+
+    def transform(self, X, Y=None):
+        if self.X_shape[1:] != X.shape[1:]:
+            raise ValueError(f"Training X has shape {self.X_shape}, while the new X has shape {X.shape}")
+
+        X = X.copy() - self.X_mean
+        X_scores = np.zeros((X.shape[0], self.n_components))
+        X_hasMiss = np.any(np.isnan(X))
+        X_miss = np.isnan(X)
+
+        for a in range(self.n_components):
+            if X_hasMiss:
+                X_scores[:, a] = miss_mmodedot(X, [ff[:, a] for ff in self.X_factors[1:]], X_miss)
+            else:
+                X_scores[:, a] = multi_mode_dot(X, [ff[:, a] for ff in self.X_factors[1:]], range(1, self.X_dim))
+            X -= outer([X_scores[:, a]] + [ff[:, a] for ff in self.X_factors[1:]])
+
+        if Y is not None:
+            Y = Y.copy()
+            # Check on the shape of Y
+            if (Y.ndim != 1) and (Y.ndim != 2):
+                raise ValueError("Only a matrix (2-mode tensor) Y is allowed.")
+            if Y.ndim == 1:
+                Y = Y.reshape((-1, 1))
+            if self.Y_shape[1:] != Y.shape[1:]:
+                raise ValueError(f"Training Y has shape {self.Y_shape}, while the new Y has shape {Y.shape}")
+
+            Y -= self.Y_mean
+            Y_scores = np.zeros((Y.shape[0], self.n_components))
+            for a in range(self.n_components):
+                Y_scores[:, a] = Y @ self.Y_factors[1][:, a]
+                Y -= X_scores @ self.coef_[:, [a]] @ self.Y_factors[1][:, [a]].T
+            return X_scores, Y_scores
+
+        return X_scores
+
+    def X_reconstructed(self):
+        return factors_to_tensor(self.X_factors) + self.X_mean
